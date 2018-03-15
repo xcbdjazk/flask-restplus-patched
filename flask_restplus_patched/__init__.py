22ba031b73f52c644a2e0c3e572f9c36afe2f439
simeon <kingshinefss@gmail.com>
2018-03-15 16:33:32 +0800

    add post json

--- flask_restplus_patched/__init__.py
+++ flask_restplus_patched/__init__.py
@@ -4,6 +4,6 @@ from flask_restplus import *
 from .api import Api
 from .model import Schema, DocumentSchema, DefaultHTTPErrorSchema
 from .namespace import Namespace
-from .parameters import Parameters, PostFormParameters, PatchJSONParameters
+from .parameters import Parameters, PostFormParameters, PostJSONParameters, PatchJSONParameters
 from .swagger import Swagger
 from .resource import Resource
