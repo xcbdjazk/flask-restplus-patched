e07bec22cb5511c3250b58e2f7a45381e8326637
simeon <kingshinefss@gmail.com>
2018-03-15 16:02:49 +0800

    modify validation error code

--- flask_restplus_patched/api.py
+++ flask_restplus_patched/api.py
@@ -16,7 +16,7 @@ class Api(OriginalApi):
 
     def init_app(self, app, **kwargs):
         super(Api, self).init_app(app, **kwargs)
-        app.errorhandler(HTTPStatus.UNPROCESSABLE_ENTITY.value)(handle_validation_error)
+        app.errorhandler(HTTPStatus.NOT_ACCEPTABLE.value)(handle_validation_error)
 
     def namespace(self, *args, **kwargs):
         # The only purpose of this method is to pass a custom Namespace class
@@ -29,6 +29,6 @@ class Api(OriginalApi):
 def handle_validation_error(err):
     exc = err.data['exc']
     return jsonify({
-        'status': HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        'status': HTTPStatus.NOT_ACCEPTABLE.value,
         'message': exc.messages
-    }), HTTPStatus.UNPROCESSABLE_ENTITY.value
+    }), HTTPStatus.NOT_ACCEPTABLE.value
