dbfaa6a70bbff83a6be94f72b35c50e3b732ba88
simeon <kingshinefss@gmail.com>
2018-03-22 14:32:12 +0800

    bugfix

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
 
     install_requires=[
         'Flask', 'flask-restplus', 'marshmallow', 'flask-marshmallow',
-        'marshmallow-mongoengine', 'webargs', 'apispec'],
+        'marshmallow-mongoengine', 'webargs', 'apispec','enum34'],
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
