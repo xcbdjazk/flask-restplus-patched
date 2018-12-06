d2268e12c80d4fd2964c8aa34e6ee4f9050b4737
simeon <kingshinefss@gmail.com>
2018-12-06 16:24:59 +0800

    Update api.py

--- flask_restplus_patched/api.py
+++ flask_restplus_patched/api.py
@@ -32,7 +32,7 @@ class Api(OriginalApi):
         :rtype: str
         '''
         result = url_for(self.endpoint('specs'), _external=True)
-        return result
+        return result[5:]
 
 # Return validation errors as JSON
 def handle_validation_error(err):
