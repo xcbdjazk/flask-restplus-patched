8b3515fce3c4e2c5dab9a3ee2364bc681915e8fd
戊 <kingshinefss@gmail.com>
2020-03-10 11:20:58 +0800

    bugfix

--- flask_restplus_patched/__init__.py
+++ flask_restplus_patched/__init__.py
@@ -1,11 +1,11 @@
 
-__version__ = '0.1.10.2020031011'
+__version__ = '0.1.10.2020031012'
 
 from flask_restplus import *
 from .api import Api
 from .model import Schema, DocumentSchema, DefaultHTTPErrorSchema
 from .namespace import Namespace
-from .parameters import Parameters, PostFormParameters, PostJSONParameters, PatchJSONParameters
+from .parameters import Parameters, PostFormParameters, PostJSONParameters, PatchJSONParameters, QueryParameters
 from .swagger import Swagger
 from .resource import Resource
 
